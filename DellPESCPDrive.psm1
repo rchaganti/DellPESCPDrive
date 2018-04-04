e2e32be58ff837b8dfc799532548c83fb4e329bb
rchaganti <ravikanth@ravichaganti.com>
2018-04-04 20:42:14 +0530

    Adding support for streaming SCP real time

--- DellPESCPDrive.psm1
+++ DellPESCPDrive.psm1
@@ -94,24 +94,54 @@ class DellPEServerComponentAttribute : SHiPSDirectory
 }
 
 #region support functions
+function Get-PEServer
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ServiceTag
+    )
+
+    [DellPEServerRoot]::availableServers | Where-Object {$_.SystemConfiguration.ServiceTag -eq $ServiceTag}
+}
+
 function Connect-PEServer
 {
     [CmdletBinding()]
     param 
     (
-        [Parameter(Mandatory = $true)]
+        [Parameter()]
         [String]
-        $JsonPath
+        $JsonPath,
+
+        [Parameter()]
+        [String]
+        $DRACIPAddress,
+
+        [Parameter()]
+        [pscredential]
+        $DRACCredential
     )
 
-    if (Test-Path -Path $JsonPath)
+    if ($JsonPath)
     {
-        $json = Get-Content -Path $JsonPath -Raw | ConvertFrom-Json
-        [DellPEServerRoot]::availableServers += $json
+        if (Test-Path -Path $JsonPath)
+        {
+            $json = Get-Content -Path $JsonPath -Raw | ConvertFrom-Json
+        }
+        else
+        {
+            throw "$JsonPath not found."
+        }
     }
     else
     {
-        throw "$JsonPath not found."
+        $json = Get-PEServerSCP -DRACIPAddress $DRACIPAddress -DRACCredential $DRACCredential        
+    }
+
+    if (-not (Get-PEServer -ServiceTag $json.SystemConfiguration.ServiceTag))
+    {
+        [DellPEServerRoot]::availableServers += $json
     }
 }
 
@@ -132,4 +162,71 @@ function Get-ComponentAttribute
     $attributeObject = $serverCP.SystemConfiguration.Components.Where({$_.FQDD -eq $ComponentFQDD}).Attributes
     return $attributeObject
 }
+
+Function Get-PEServerSCP
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [String]
+        $DRACIPAddress,
+
+        [Parameter(Mandatory = $true)]
+        [pscredential]
+        $DRACCredential
+    )
+
+add-type @'
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+public bool CheckValidationResult(
+    ServicePoint srvPoint, X509Certificate certificate,
+    WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+'@
+    if (([System.Net.ServicePointManager]::CertificatePolicy).ToString() -ne 'TrustAllCertsPolicy')
+    {
+        [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy -ErrorAction Stop
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12
+    }
+
+    $headerObject = @{
+        'ExportFormat'    = 'JSON'
+        'ShareParameters' = @{'Target'='All'}
+    }
+
+    $headerJson = $headerObject | ConvertTo-Json
+
+    $url = "https://$DRACIPAddress/redfish/v1/Managers/iDRAC.Embedded.1/Actions/Oem/EID_674_Manager.ExportSystemConfiguration"
+    $response = Invoke-WebRequest -Uri $url -Credential $DRACCredential -Method Post -Body $headerJson -ContentType 'application/json' -ErrorVariable scpError
+
+    $responseJson = $response.RawContent | ConvertTo-Json
+    $jobID = ([regex]::Match($responseJson,'JID_.+?r').captures.groups[0].value).replace('\r','')
+
+    $url = "https://$DRACIPAddress/redfish/v1/TaskService/Tasks/$jobID"
+    $response = Invoke-WebRequest -Uri $url -Credential $DRACCredential -Method Get -UseBasicParsing -ContentType 'application/json'
+
+    $responseObject = $response.Content | ConvertFrom-Json
+
+    While($responseObject.TaskState -eq 'Running')
+    {
+        Start-Sleep -Seconds 5
+        $response = Invoke-WebRequest -Uri $url -Credential $DRACCredential -Method Get -UseBasicParsing -ContentType 'application/json'
+        $responseObject = $response.Content | ConvertFrom-Json
+    }
+
+    if ($response.StatusCode -eq 200 -and ($response.RawContent.Contains('SystemConfiguration')))
+    {
+        return ($response.Content | ConvertFrom-Json)
+    }
+    else
+    {
+        throw 'Error in exporting SCP'
+    }
+}
+
 #endregion
